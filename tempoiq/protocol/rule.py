ea7368d5d9d91534fe2c6fff89b85e4f9753a684
Aaron Brenzel <aaron.brenzel@tempo-db.com>
2014-08-27 12:08:13 -0500

    definition of monitoring data types

---
+++ tempoiq/protocol/rule.py
@@ -0,0 +1,41 @@
+import copy
+
+
+class Rule(object):
+    def __init__(self, name, alert_by=None, key=None, selection=None,
+                 conditions=[], action=None):
+        self.name = name
+        self.alert_by = alert_by
+        self.key = key
+        self.selection = selection
+        #make sure to avoid mutability of kwargs that default to list here
+        self.conditions = copy.deepcopy(conditions)
+        self.action = action
+
+
+class Trigger(object):
+    def __init__(self, trigger_type, args):
+        self.trigger_type = trigger_type
+        self.args = args
+
+
+class Filter(object):
+    def __init__(self, inclusion, filter_type, args):
+        self.inclusion = inclusion
+        self.filter_type = filter_type
+        self.args = args
+
+
+class Condition(object):
+    def __init__(self, filters, trigger):
+        self.filters = filters
+        self.trigger = trigger
+
+
+class Action(object):
+    pass
+
+
+class Webhook(Action):
+    def __init__(self, url):
+        self.url = url
