8ae5fce272aafd4a8cd9539f5d0feea58b5779a6
Aaron Brenzel <aaron.brenzel@tempo-db.com>
2014-09-09 15:31:04 -0500

    tempoiq response objects for all basic client methods

--- tempoiq/client.py
+++ tempoiq/client.py
@@ -1,86 +1,9 @@
-import functools
-import urlparse
-import urllib
 import json
-import endpoint
-import protocol
+import urlparse
 from protocol.encoder import WriteEncoder, CreateEncoder, ReadEncoder
 from protocol.query.builder import QueryBuilder
-from response import Response, SensorPointsResponse, ResponseException
+from response import Response, SensorPointsResponse
 from response import RuleResponse, DeviceResponse
-
-
-def make_series_url(key):
-    """For internal use. Given a series key, generate a valid URL to the series
-    endpoint for that key.
-
-    :param string key: the series key
-    :rtype: string"""
-
-    url = urlparse.urljoin(endpoint.SERIES_ENDPOINT, 'key/')
-    url = urlparse.urljoin(url, urllib.quote_plus(key))
-    return url
-
-
-class with_response_type(object):
-    """For internal use. Decorator for ensuring the Response object returned by
-    the :class:`Client` object has a data attribute that corresponds to the
-    object type expected from the TempoDB API.  This class should not be
-    used by user code.
-
-    The "t" argument should be a string corresponding to the name of a class
-    from the :mod:`tempodb.protocol.objects` module, or a single element list
-    with the element being the name of a class from that module if the API
-    endpoint will return a list of those objects.
-
-    :param t: the type of object to cast the TempoDB response to
-    :type t: list or string"""
-
-    def __init__(self, t):
-        self.t = t
-
-    def __call__(self, f, *args, **kwargs):
-        @functools.wraps(f)
-        def wrapper(*args, **kwargs):
-            resp = f(*args, **kwargs)
-            #dont try this at home kids
-            session = args[0].session
-            resp_obj = Response(resp, session)
-            if resp_obj.status == 200:
-                resp_obj._cast_payload(self.t)
-            else:
-                raise ResponseException(resp_obj)
-            return resp_obj
-        return wrapper
-
-
-class with_cursor(object):
-    """For internal use. Decorator class for automatically transforming a
-    response into a Cursor of the required type.
-
-    :param class cursor_type: the cursor class to use
-    :param class data_type: the data type that cursor should generate"""
-
-    def __init__(self, cursor_type, data_type):
-        self.cursor_type = cursor_type
-        self.data_type = data_type
-
-    def __call__(self, f, *args, **kwargs):
-        @functools.wraps(f)
-        def wrapper(*args, **kwargs):
-            resp = f(*args, **kwargs)
-            session = args[0].session
-            resp_obj = Response(resp, session)
-            if resp_obj.status == 200:
-                data = json.loads(resp_obj.body)
-                if self.cursor_type in [protocol.SeriesCursor,
-                                        protocol.SingleValueCursor]:
-                    return self.cursor_type(data, self.data_type, resp_obj)
-                else:
-                    return self.cursor_type(data, self.data_type, resp_obj,
-                                            kwargs.get('tz'))
-            raise ResponseException(resp_obj)
-        return wrapper
 
 
 class MonitoringClient(object):
@@ -89,7 +12,8 @@ class MonitoringClient(object):
 
     def delete_rule(self, key):
         url = urlparse.urljoin(self.endpoint.base_url, 'monitors/' + key)
-        return self.endpoint.delete(url)
+        resp = self.endpoint.delete(url)
+        return Response(resp, self.endpoint)
 
     def get_annotations(self, key):
         url = urlparse.urljoin(self.endpoint.base_url,
@@ -98,11 +22,12 @@ class MonitoringClient(object):
 
     def get_changelog(self, key):
         url = urlparse.urljoin(self.endpoint.base_url,
-                               'monitors/changes/' + key)
+                               'monitors/%s/changes/' % key)
         return self.endpoint.get(url)
 
     def get_logs(self, key):
-        url = urlparse.urljoin(self.endpoint.base_url, 'monitors/logs/' + key)
+        url = urlparse.urljoin(self.endpoint.base_url,
+                               'monitors/%s/logs/' % key)
         return self.endpoint.get(url)
 
     def get_rule(self, key):
@@ -110,7 +35,8 @@ class MonitoringClient(object):
         return RuleResponse(self.endpoint.get(url), self)
 
     def get_usage(self, key):
-        url = urlparse.urljoin(self.endpoint.base_url, 'monitors/usage/' + key)
+        url = urlparse.urljoin(self.endpoint.base_url,
+                               'monitors/%s/usage/' % key)
         return self.endpoint.get(url)
 
     def list_rules(self):
@@ -135,12 +61,14 @@ class Client(object):
     def delete_device(self, query):
         url = urlparse.urljoin(self.endpoint.base_url, 'devices/')
         j = json.dumps(query, default=self.read_encoder.default)
-        return self.endpoint.delete(url, j)
+        resp = self.endpoint.delete(url, j)
+        return Response(resp, self.endpoint)
 
     def monitor(self, rule):
         url = urlparse.urljoin(self.endpoint.base_url, 'monitors/')
         rule_json = json.dumps(rule, default=self.write_encoder.default)
-        return self.endpoint.post(url, rule_json)
+        resp = self.endpoint.post(url, rule_json)
+        return RuleResponse(resp, self.endpoint)
 
     def query(self, object_type):
         return QueryBuilder(self, object_type)
@@ -160,5 +88,6 @@ class Client(object):
     def write(self, write_request):
         url = urlparse.urljoin(self.endpoint.base_url, 'write/')
         default = self.write_encoder.default
-        return self.endpoint.post(url, json.dumps(write_request,
+        resp = self.endpoint.post(url, json.dumps(write_request,
                                                   default=default))
+        return Response(resp, self.endpoint)
--- tests/test_decoder.py
+++ tests/test_decoder.py
@@ -142,3 +142,52 @@ class TestTempoIQDecoder(unittest.TestCase):
         self.assertEquals(decoded.key, 'foo')
         self.assertEquals(len(decoded.conditions), 1)
         self.assertTrue(isinstance(decoded.action, Webhook))
+
+    def test_decoder_for_list_of_rules(self):
+        search = {
+            'select': 'sensors',
+            'filters': {
+                'devices': {'key': 'foo'},
+                'sensors': {'key': 'bar'}
+            }
+        }
+        rule = {
+            'name': 'one rule to rule them all',
+            'key': 'foo',
+            'action': {
+                'url': 'http://www.foo.bar'
+            },
+            'conditions': [
+                {
+                    'trigger': {
+                        'name': 'exp_moving_average',
+                        'arguments': [
+                            'static',
+                            'lt',
+                            '300',
+                            '5'
+                        ]
+                    },
+                    'filter': {
+                        'and': [
+                            {
+                                'operation': 'select',
+                                'type': 'device_key',
+                                'arguments': ['foo']
+                            }
+                        ]
+                    }
+                }
+            ]
+        }
+        j = {
+            'rule': rule,
+            'search': search,
+            'alerts': 'any'
+        }
+        decoder = TempoIQDecoder()
+        decoded = json.loads(json.dumps([j]), object_hook=decoder)
+        self.assertEquals(decoded[0].alert_by, 'any')
+        self.assertEquals(decoded[0].key, 'foo')
+        self.assertEquals(len(decoded[0].conditions), 1)
+        self.assertTrue(isinstance(decoded[0].action, Webhook))
