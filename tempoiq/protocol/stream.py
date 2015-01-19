c9a5b0ea7e813dcb4287c5960e94a358915ea3b2
Aaron Brenzel <aaron.brenzel@tempo-db.com>
2015-01-19 14:55:46 -0600

    add comment to make clear what this is for

--- tempoiq/protocol/stream.py
+++ tempoiq/protocol/stream.py
@@ -1,6 +1,8 @@
 from query.selection import ScalarSelectable
 
 
+#used for enabling the search DSL for substream selection, not up for release
+#yet
 class Stream(object):
     function = ScalarSelectable('streams', 'function')
 
